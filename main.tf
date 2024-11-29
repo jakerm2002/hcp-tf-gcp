610a86c7cff09459229f1b4e7ce5c8729dde2378
Jake Medina <jake.medina02@gmail.com>
2024-11-28 19:00:10 -0600

    re add workload identity pools

--- main.tf
+++ main.tf
@@ -21,31 +21,31 @@ resource "google_project_service" "gcp_services" {
   service = each.key
 }
 
-# data "google_project" "project" {
-# }
+data "google_project" "project" {
+}
 
-# # create a workload identity pool for HCP Terraform
-# resource "google_iam_workload_identity_pool" "hcp_tf" {
-#   project                   = data.google_project.project.number
-#   workload_identity_pool_id = "hcp-tf-pool"
-#   display_name              = "HCP Terraform Pool"
-#   description               = "Used to authenticate to Google Cloud"
-# }
+# create a workload identity pool for HCP Terraform
+resource "google_iam_workload_identity_pool" "hcp_tf" {
+  project                   = data.google_project.project.number
+  workload_identity_pool_id = "hcp-tf-pool"
+  display_name              = "HCP Terraform Pool"
+  description               = "Used to authenticate to Google Cloud"
+}
  
-# # create a workload identity pool provider for HCP Terraform
-# resource "google_iam_workload_identity_pool_provider" "hcp_tf" {
-#   project                            = data.google_project.project.number
-#   workload_identity_pool_id          = google_iam_workload_identity_pool.hcp_tf.workload_identity_pool_id
-#   workload_identity_pool_provider_id = "hcp-tf-provider"
-#   display_name                       = "HCP Terraform Provider"
-#   description                        = "Used to authenticate to Google Cloud"
-#   attribute_condition                = "assertion.terraform_organization_name==\"${local.organization_name}\""
-#   attribute_mapping = {
-#     "google.subject"                     = "assertion.sub"
-#     "attribute.terraform_workspace_id"   = "assertion.terraform_workspace_id"
-#     "attribute.terraform_full_workspace" = "assertion.terraform_full_workspace"
-#   }
-#   oidc {
-#     issuer_uri = "https://app.terraform.io"
-#   }
-# }
+# create a workload identity pool provider for HCP Terraform
+resource "google_iam_workload_identity_pool_provider" "hcp_tf" {
+  project                            = data.google_project.project.number
+  workload_identity_pool_id          = google_iam_workload_identity_pool.hcp_tf.workload_identity_pool_id
+  workload_identity_pool_provider_id = "hcp-tf-provider"
+  display_name                       = "HCP Terraform Provider"
+  description                        = "Used to authenticate to Google Cloud"
+  attribute_condition                = "assertion.terraform_organization_name==\"${local.organization_name}\""
+  attribute_mapping = {
+    "google.subject"                     = "assertion.sub"
+    "attribute.terraform_workspace_id"   = "assertion.terraform_workspace_id"
+    "attribute.terraform_full_workspace" = "assertion.terraform_full_workspace"
+  }
+  oidc {
+    issuer_uri = "https://app.terraform.io"
+  }
+}
