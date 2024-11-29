cc71501680c22303501839b157c4b8d8395801b2
Jake Medina <jake.medina02@gmail.com>
2024-11-28 19:34:38 -0600

    add environment variables for workload identity federation

--- main.tf
+++ main.tf
@@ -81,3 +81,39 @@ resource "google_service_account_iam_member" "hcp_workload_identity_user" {
 #   role    = "roles/storage.admin"
 #   project = local.google_project_id
 # }
+
+# create a variable set to store the workload identity federation config for the 'example' service account
+resource "tfe_variable_set" "wip_variable_set" {
+  name         = google_service_account.hcp_tf.account_id
+  description  = "Workload identity federation configuration for ${google_service_account.hcp_tf.name}"
+  organization = local.organization_name
+}
+ 
+# # share the variable set with another HCP Terraform Workspace
+# resource "tfe_workspace_variable_set" "example" {
+#   variable_set_id = tfe_variable_set.example.id
+#   workspace_id    = "ws-XXXXXXXXXXXXXXX"
+# }
+
+resource "tfe_variable" "hcp_tf_provider_auth" {
+  key             = "HCP_TF_GCP_PROVIDER_AUTH"
+  value           = "true"
+  category        = "env"
+  variable_set_id = tfe_variable_set.wip_variable_set.id
+}
+ 
+resource "tfe_variable" "hcp_tf_service_account_email" {
+  sensitive       = true
+  key             = "HCP_TF_GCP_RUN_SERVICE_ACCOUNT_EMAIL"
+  value           = google_service_account.example.email
+  category        = "env"
+  variable_set_id = tfe_variable_set.wip_variable_set.id
+}
+ 
+resource "tfe_variable" "hcp_tf_provider_name" {
+  sensitive       = true
+  key             = "HCP_TF_GCP_WORKLOAD_PROVIDER_NAME"
+  value           = google_iam_workload_identity_pool_provider.hcp_tf.name
+  category        = "env"
+  variable_set_id = tfe_variable_set.wip_variable_set.id
+}
